2472ea58f3631cbcebf3b6a3369ff33f4b736be1
testgithubrit-create <115694019+testgithubrit-create@users.noreply.github.com>
2022-10-27 17:46:00 +0530

    Add files via upload

---
+++ ReplaceExample.java
@@ -0,0 +1,7 @@
+public class ReplaceExample {
+    public static void main(String args[]){
+        String s1="java string pool refers to collection of strings which are stored in heap memory";
+        String replaceString=s1.replace("java","c language");
+        System.out.println(replaceString);
+    }}
+
