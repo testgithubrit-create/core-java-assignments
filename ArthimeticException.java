3a55089ea3bedf8ea977fe962856502744574c1c
testgithubrit-create <115694019+testgithubrit-create@users.noreply.github.com>
2022-10-27 17:42:37 +0530

    Add files via upload

---
+++ ArthimeticException.java
@@ -0,0 +1,26 @@
+
+public class ArthimeticException {
+    public static void main(String args[]) {
+        int a, b, c;
+        a = 10;
+        b = 0;
+        try {
+            c = a / 10;
+            System.out.println(c);
+        }
+        catch(ArithmeticException e) {
+            System.out.println("not possible of divided by zero");
+        }
+        }
+
+        }
+
+
+
+
+
+
+
+
+
+
