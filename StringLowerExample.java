4e3dd6eee57af0bc8f1303c517ba765c28a071df
testgithubrit-create <115694019+testgithubrit-create@users.noreply.github.com>
2022-10-27 17:47:13 +0530

    Add files via upload

---
+++ StringLowerExample.java
@@ -0,0 +1,7 @@
+public class StringLowerExample {
+    public static void main(String args[]){
+        String s1="JAVA STRING POOL REFERS TO COLLECTION OF STRINGS WHICH ARE STORED IN HEAP MEMORY";
+        String s1lower=s1.toLowerCase();
+        System.out.println(s1lower);}
+}
+
