c6fef7a2e8e1ba6c8fef9dcdf5902a1c4af373cb
testgithubrit-create <115694019+testgithubrit-create@users.noreply.github.com>
2022-10-27 17:48:01 +0530

    Add files via upload

---
+++ StringUpperExample.java
@@ -0,0 +1,7 @@
+public class StringUpperExample {
+    public static void main(String args[]){
+        String s1="java string pool refers to collection of strings which are stored in heap memory";
+        String s1upper=s1.toUpperCase();
+        System.out.println(s1upper);}
+}
+
