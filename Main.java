9e42ade282caefee50fe8231d9c59f3a0ff662ce
testgithubrit-create <115694019+testgithubrit-create@users.noreply.github.com>
2022-10-27 17:45:28 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String args[]){
+        try{
+            int divideByZero = 5/0;
+            System.out.println("rest of code in try block");
+        }
+        catch (ArithmeticException e){
+            System.out.println("ArithmeticException => " + e.getMessage());
+        }
+    }
+}
+
