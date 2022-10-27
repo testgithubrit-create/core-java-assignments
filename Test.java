29798ce5ff7554f94e7ea5cbe5f3659f4ea6cb10
testgithubrit-create <115694019+testgithubrit-create@users.noreply.github.com>
2022-10-27 17:48:28 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String args[]){
+        String str = "Hello world";
+        System.out.println("the size of " + "the String is " + str.length());
+
+    }
+}
