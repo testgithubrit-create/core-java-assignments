fa6c028d3c6b5ba3abec3e5a572027bbd21c144b
testgithubrit-create <115694019+testgithubrit-create@users.noreply.github.com>
2022-10-27 17:43:28 +0530

    Add files via upload

---
+++ ConcatExample.java
@@ -0,0 +1,9 @@
+public class ConcatExample {
+    public static void main(String args[]){
+        String s1="java string";
+        s1.concat("Hello");
+        System.out.println(s1);
+        s1=s1.concat("Hello How are you ");
+        System.out.println(s1);
+    }}
+
